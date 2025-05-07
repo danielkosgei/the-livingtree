2f48dd2d6e1cdee8b7d632b465322a81881c72b8
Daniel Kosgei <dannykosgei@gmail.com>
2025-05-07 22:28:03 +0300

    added login form with shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            
             {children}
           </ThemeProvider>
         </body>
